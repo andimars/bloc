6df7a28f2e33df67631484182e79b49398c12f45
andimars <andimars84@gmail.com>
2023-03-31 14:22:01 +0700

    menambah folder bloc_provider

---
+++ bloc_provider/lib/pages/data_widget.dart
@@ -0,0 +1,18 @@
+import 'package:bloc_builder/pages/center_widget.dart';
+import 'package:flutter/material.dart';
+
+class DataWidget extends StatelessWidget {
+  const DataWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: 200,
+      color: Colors.red,
+      child: CenterWidget(),
+    );
+  }
+}
---
+++ bloc_provider/lib/pages/home.dart
@@ -0,0 +1,72 @@
+import 'package:bloc_builder/bloc/counter.dart';
+import 'package:bloc_builder/pages/data_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Counter mycounter = BlocProvider.of<Counter>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text('Flutter Bloc - Bloc Provider'),
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              // Button -
+              Material(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(15),
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(15),
+                  onTap: () {
+                    mycounter.decrement();
+                  },
+                  child: const SizedBox(
+                    height: 100,
+                    width: 70,
+                    child: Center(
+                      child: Icon(
+                        Icons.remove,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              // Widget data counter
+              DataWidget(),
+              // button +
+              Material(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(15),
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(15),
+                  onTap: () {
+                    mycounter.increment();
+                  },
+                  child: const SizedBox(
+                    height: 100,
+                    width: 70,
+                    child: Center(
+                      child: Icon(
+                        Icons.add,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
